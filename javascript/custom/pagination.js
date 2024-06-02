33a2334396df95a5a07dd68965d61e392071639b
Geyser Velasquez <gvelasquez.9312@unimar.edu.ve>
2024-06-02 12:50:04 -0400

    Solucion de Bug de Paginacion Multiple

--- javascript/custom/pagination.js
+++ javascript/custom/pagination.js
@@ -2,9 +2,10 @@ import { applyFilters } from './filter.js';
 
 export let currentPage = 0;
 
-export function setupPagination() {
-    const paginationItems = document.getElementsByClassName("skip-page");
+const paginationItems = document.getElementsByClassName("skip-page");
 
+export function setupPagination() {
+    
     for (let i = 0; i < paginationItems.length; i++) {
         paginationItems[i].addEventListener("click", function () {
             console.log("e mano: " + i);
@@ -13,23 +14,23 @@ export function setupPagination() {
             updatePaginationClass();
         });
     }
-
-    document.getElementById("btn-prev").addEventListener("click", () => {
-        if (currentPage > 0) {
-            currentPage--;
-            applyFilters();
-            updatePaginationClass();
-        }
-    });
-
-    document.getElementById("btn-next").addEventListener("click", () => {
-        if (currentPage < paginationItems.length - 1) { // Adjust based on the length of pagination items
-            currentPage++;
-            applyFilters();
-            updatePaginationClass();
-        }
-    });
 }
+
+document.getElementById("btn-prev").addEventListener("click", () => {
+    if (currentPage > 0) {
+        currentPage--;
+        applyFilters();
+        updatePaginationClass();
+    }
+});
+
+document.getElementById("btn-next").addEventListener("click", () => {
+    if (currentPage < paginationItems.length - 1) { // Adjust based on the length of pagination items
+        currentPage++;
+        applyFilters();
+        updatePaginationClass();
+    }
+});
 
 function updatePaginationClass() {
     $(".skip-page").removeClass("page-active");
