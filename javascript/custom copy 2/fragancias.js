2261ba2bb181863dc97c644906c5b056b605d9a8
Geyser Velasquez <gvelasquez.9312@unimar.edu.ve>
2024-06-30 16:40:07 -0400

    Reajuste del Footer

--- javascript/custom copy 2/fragancias.js
+++
@@ -1,8 +0,0 @@
-import { initializeFilters } from './filter.js';
-import { setupPagination } from './pagination.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  initializeFilters();
-  setupPagination();
-  console.log("e mano")
-});
